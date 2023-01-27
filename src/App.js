cab6f8938e11042fb10911f6dcc62c07126332ef
ismailsaoudi <ismail.saoudi@icloud.com>
2023-01-27 11:06:52 -0500

    made ir llok better

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Home from "./components/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./style.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   const [load, updateLoad] = useState(true);
